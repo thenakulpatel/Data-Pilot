5ad5f17e7e4c4065d4ce98efe2693acd3044b50d
thenakulpatel <nakuljpatel1@gmail.com>
2026-06-19 10:51:31 +0530

    transformer issue solved

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig }
+from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
+  transpilePackages: [
+    "@xenova/transformers",
+  ],
 };
 
 export default nextConfig;
